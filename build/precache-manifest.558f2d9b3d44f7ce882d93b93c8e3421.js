9b120150d0dd5ae96f72c61995537260dda22b5d
bellomusodiq <bmayowa25@gmail.com>
2020-07-06 12:18:22 +0100

    added react build folder

---
+++ build/precache-manifest.558f2d9b3d44f7ce882d93b93c8e3421.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "892ae7acc3eb0009b2a8b48116295c57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1da4bd695d082ac149a0",
+    "url": "/static/css/main.d1efd317.chunk.css"
+  },
+  {
+    "revision": "f682b38c46819af5b3d7",
+    "url": "/static/js/2.d65041e7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d65041e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1da4bd695d082ac149a0",
+    "url": "/static/js/main.17274cfa.chunk.js"
+  },
+  {
+    "revision": "42b9b8c7660d7a7faa5b",
+    "url": "/static/js/runtime-main.39f10920.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
